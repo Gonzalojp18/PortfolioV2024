d20ec468f7d07fa2cd692f5a76b353e2676d5dea
Gonzalojp18 <pgonzalojose@gmail.com>
2022-12-10 23:11:04 -0300

    portfolio

---
+++ js/funcion.js
@@ -0,0 +1,31 @@
+// gsap.registerPlugin('scrollTrigger');
+
+// gsap.to(".center", {
+//     // x: '10%',
+//     duration: 3,
+//     delay: 2,
+//     ease: "power2.inOut",
+// });
+
+// gsap.to(".fEnd", {
+//     // translate: '-10%',
+//     duration: 3,
+//     delay: 2,
+//     ease: "power2.inOut",
+// });
+
+// let tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".center",
+//         markers: true,
+//         start: "top top", // when the top of the trigger hits the top of the viewport
+//         end: "100% 100%", // end after scrolling 500px beyond the start
+//         scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     }
+// });
+
+
+// tl.to('.boxOne', {translate: '2rem', y: '2rem', duration: 3, ease: "power2.inOut"})
+// tl.to('.boxTwo', {translate: '-10%', y: '5h', duration: 3})
+// tl.to('.boxThree', {translate: '10%', y: '5h', duration: 3})
+// tl.to('.boxFour', {translate: '-10%', y: '5h', duration: 3})
